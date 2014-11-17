cfa32d888b6ba8231ac0afe207f1a679fd08536c
ChloeCalvarin <chloe.calvarin@gmail.com>
2014-11-16 20:38:49 -0800

    Stuck trying to get java to recognize the instrument classes.

--- src/tutorials/Bing.java
+++ src/tutorials/Bing.java
@@ -4,6 +4,7 @@ package tutorials;
 import jm.JMC;
 import jm.music.data.*;
 import jm.util.*;
+import jm.audio.*;
 
 /**
 * This is the simplest jMusic program of all, based off of tutorial's sample 
@@ -11,13 +12,15 @@ import jm.util.*;
 * The equivalent to a programming language's 'Hello World.
 * 
 * Chloe Calvarin
-* 	I've used this file to get a sense of the mea
+* 	I've used this file to get a sense of the jmusic scene, combining multiple 
+*   tutorials into one
 */
 
 public final class Bing implements JMC{
 
 	public static void main(String[] args){
-		//create a middle C minim (half note)
+		//create a bunch of Cs minim (half note)
+		Note c5 = new Note(C5, MINIM);
 		Note c4 = new Note(C4, MINIM);
 		Note c3 = new Note(C3, MINIM);
 		Note c2 = new Note(C2, MINIM);
@@ -26,6 +29,7 @@ public final class Bing implements JMC{
 		Phrase phr = new Phrase();
 		
 		//put the note inside the phrase
+		phr.addNote(c5);
 		phr.addNote(c3);
 		phr.addNote(c2);
 		phr.addNote(c4);
@@ -41,6 +45,8 @@ public final class Bing implements JMC{
 		//write the score as a MIDI file to disk, and then play it back
 		Write.midi(s,"Bing.mid");
 		Play.mid("Bing.mid");
+		Instrument inst = new SawtoothInst(44100);
+		Write.au(s, inst);
 	}
 
 }
